cc3df9c2b62a64d4bd42c4aa4010a14e20d536bd
Jordan Victor <jordanvictor63@gmail.com>
2025-05-20 22:57:24 -0400

    Deploy para GitHub Pages

---
+++ src/views/CadastroView.vue
@@ -0,0 +1,56 @@
+<template>
+    <main class="container-lg">
+        <header>
+            <h4>INFORMAÇÕES DA EMPRESA</h4>
+        </header>
+
+        <form class="cadastro" @submit.prevent="prevent">
+            <section class="d-flex gap-3">
+                <div>
+                    <div class="text-start">Logo da empresa</div>
+                    <input type="file" class="form-control" id="logo" name="logo" placeholder="Opcional">
+                </div>
+
+                <div>
+                    <div class="text-start">Nome da sua empresa</div>
+                    <input type="text" class="form-control" id="nome_estabelecimneto" name="nome_estabelecimento" required>
+                </div>
+
+                <div>
+                    <div class="text-start">Ramo</div>
+                    <input type="text" class="form-control" id="ramo" name="ramo" required>
+                </div>
+
+                <div>
+                    <div class="text-start">Horário de funcionamento</div>
+                    <input type="date" class="form-control" id="hora_funci" name="hora_funci" required>
+                </div>
+            </section>
+
+            <button class="btn bg-secondary text-white">Registrar dados</button>
+        </form>
+    </main>
+</template>
+
+<script>
+    export default {
+        name:'CadastroView',
+        data() {
+            return {
+                
+            }
+        },
+
+        methods: {
+            prevent(){
+                alert("Empresa cadastrada")
+            }
+        },
+    }
+</script>
+
+<style scoped>
+label{
+    margin-left: 0px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+  <header>
+    <LinksView/>
+  </header>
+
+    <router-link :to="{name:'cadastro'}">
+      <div class="card">
+        CADASTRO SEU ESTABELECIMENTO
+      </div>
+    </router-link>
+  </main>
+</template>
+
+<script>
+// @ is an alias to /src
+import LinksView from '../components/LinksView.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    LinksView
+  }
+}
+</script>
