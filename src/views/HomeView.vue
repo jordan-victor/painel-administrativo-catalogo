b61a562b62b21e5f5bff8e593c9692827acc58b4
Jordan Victor <jordanvictor63@gmail.com>
2025-05-31 12:54:01 -0400

    Adição de cards na home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,59 @@
 <template>
-  <main>
   <header>
     <LinksView/>
   </header>
 
-    <router-link :to="{name:'cadastro'}">
-      <div class="card">
-        CADASTRO SEU ESTABELECIMENTO
-      </div>
-    </router-link>
+
+  <main class="container">
+    <!-- CARDS -->
+    <div class="cards d-flex gap-2 flex-wrap justify-content-center align-items-center align-items-center">
+      <!--  registro do estabelecimento -->
+      <router-link :to="{name:'cadastro'}">
+        <div class="card border-0 shadow p-2">
+          <!-- <i class="fa-solid fa-shop"></i> -->
+          <img src="img/store.png" alt="ícone loja">
+  
+          <span>LOJA</span>
+        </div>
+      </router-link>
+
+
+
+      <!--  registro do estabelecimento -->
+      <router-link :to="{name:'cadastro'}">
+        <div class="card border-0 shadow p-2">
+          <i class="fa-solid fa-table-list"></i>
+          <span>PEDIDOS</span>
+        </div>
+      </router-link>
+
+
+
+      <!--  registro do estabelecimento -->
+      <router-link :to="{name:'cadastro'}">
+        <div class="card border-0 shadow p-2">
+          <i class="fa-solid fa-shop"></i>
+          <span>CATÁLOGO</span>
+        </div>
+      </router-link>
+
+
+
+      <!--  registro do estabelecimento -->
+      <router-link :to="{name:'cadastro'}">
+        <div class="card border-0 shadow p-2">
+          <i class="fa-solid fa-shop"></i>
+          <span>RELATÓRIOS</span>
+        </div>
+      </router-link>
+    </div>
   </main>
 </template>
+
+
+
+
+
 
 <script>
 // @ is an alias to /src
@@ -23,3 +66,44 @@ export default {
   }
 }
 </script>
+
+
+
+
+
+<style scoped>
+  /* Estilo das fontes */
+  span{
+    text-decoration: none;
+    line-height: 20px;
+    color: rgb(61, 61, 61);
+    text-decoration: none;
+  }
+  a{
+    text-decoration: none;
+  }
+  i{
+    color: rgb(61, 61, 61);
+    font-size: 35px;
+  }
+
+  /* Estilo dos cards */
+  .cards{
+    max-width: 250px;
+    margin: 0 auto;
+  }
+  .card{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 5px;
+    width: 120px;
+  }
+
+
+  /* Estilo das imagens */
+  img{
+    width: 50px;
+  }
+</style>
